aacce7d458cfc6002bbbdd4fa7eafab18155e966
Raj <ravirajkakati@yahoo.com>
2023-02-19 18:14:47 +1100

    Update streamlit_app.py
    Added index to replicate tutorial and fixed the spelling of 'fruits'

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 # streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick listhere so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruites:", list(my_fruit_list))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # display the table on the page
 streamlit.dataframe(my_fruit_list)
