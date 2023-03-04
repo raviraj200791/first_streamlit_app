8d19bbb2b78ac14ddc594556489a46324ab0b4f5
Raj <ravirajkakati@yahoo.com>
2023-03-04 13:09:04 +1100

    indentation after else

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,12 +38,12 @@ try:
     if not fruit_choice:
         streamlit.error("Please a select a fruit to get information.")
     else:
-    streamlit.write('The user entered', fruit_choice)
-    fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
-    # take the json version of the response and normalise
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    #output it the screen as a table
-    streamlit.dataframe(fruityvice_normalized)
+        streamlit.write('The user entered', fruit_choice)
+        fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
+        # take the json version of the response and normalise
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        #output it the screen as a table
+        streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
     streamlit.errot()
